171e8dc603d1ee17ceaa49d6e1aa5d5442ff2a4b
Herbert Braun <wortwart@woerter.de>
2018-05-04 11:24:04 +0200

    Fix line endings in shell script

--- v4 - push from backend/keygen.sh
+++ v4 - push from backend/keygen.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 openssl ecparam -name prime256v1 -genkey -out key.pem
 openssl ec -in key.pem -pubout -outform DER | dd bs=1 skip=$((2+2+19+2+1)) | base64 > pubkey.b64.txt
 openssl ec -in key.pem -outform DER | dd bs=1 skip=$((2+2+1+2)) count=32 | base64 > privkey.b64.txt
