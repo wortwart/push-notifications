1af5927d3daac725c76d31b2cfb42f086ca7f858
Herbert Braun <wortwart@woerter.de>
2018-07-09 16:13:06 +0200

    Check input for sanity

--- v4 - push from backend/subscribe.php
+++ v4 - push from backend/subscribe.php
@@ -10,10 +10,14 @@ if (count($_POST)) {
 	exit;
 }
 
-$json = file_get_contents('php://input') or exit;
-$subscription = json_decode($json, false, 4, JSON_BIGINT_AS_STRING) or exit;
+$json = file_get_contents('php://input') or die('No data');
+$subscription = json_decode($json, false, 4, JSON_BIGINT_AS_STRING) or die('Invalid subscription');
+foreach ([$subscription->endpoint, $subscription->keys->p256dh, $subscription->keys->auth] as $val) {
+	if (!preg_match('/^[\w:\/.=-]+$/', $val)) die('Invalid subscription data: ' . $val);
+}
 if (!isset($subscription->expirationTime))
 	$subscription->expirationTime = null;
+
 $sth = $dbh->prepare('INSERT subscriptions (endpoint, expirationTime, p256dh, auth) VALUES (:endpoint, :expirationTime, :p256dh, :auth)');
 $sth->bindValue(':endpoint', $subscription->endpoint, PDO::PARAM_STR);
 $sth->bindValue(':expirationTime', $subscription->expirationTime, PDO::PARAM_INT);
